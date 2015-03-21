fcd1fac2af2cb48266edc93b381233f32c886ad3
Josh Owen <josh@openfolio.com>
2015-03-21 19:12:47 -0400

    only return playable songs

--- tweejay/music.py
+++ tweejay/music.py
@@ -4,12 +4,11 @@ import spotipy
 def get_spotify_track_uri(query):
     sp = spotipy.Spotify()
 
-    results = sp.search(q=query, limit=50)
-    track = next((r for r in results['tracks']['items'] if r['explicit']), None)
+    results = (r for r in sp.search(q=query, limit=50)['tracks']['items'] if 'US' in r['available_markets'])
+    track = next((r for r in results if r['explicit']), None)
     if not track:
         # Go with the lame non-explicit version
-        track = next(iter(results['tracks']['items']), None)
+        track = next(results, None)
     if track:
         return track['uri']
     return None
-
