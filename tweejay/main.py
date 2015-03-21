11adb48503759991d13a948e87a2a322d63546e4
Josh Owen <josh@openfolio.com>
2015-03-21 18:01:15 -0400

    set up spotify

--- tweejay/main.py
+++ tweejay/main.py
@@ -1,18 +1,23 @@
-from tweejay import music
+import soco
+
+from tweejay.music import get_spotify_track_uri
 
 
 if __name__ == "__main__":
-    print music.get_spotify_track_uri('greenday dookie')
-    print music.get_spotify_track_uri('green day dookie')
-    print music.get_spotify_track_uri('taylor swift')
-#     sp = spotipy.Spotify()
+    sonos = soco.discover().pop()
+
+#     spotify_plugin = Spotify(sonos)
+    track_uri = get_spotify_track_uri("Notorious Juicy")
+#     track_uri = 'spotify:track:3dbCYM0LJR8tiL0W1XXmpK'
 #
-#     results = sp.search(q='Notorious Juicy', limit=20)
-#     track = next((r for r in results['tracks']['items'] if r['explicit']), None)
-#     if not track:
-#         # Go with the lame non-explicit version
-#         track = next(results['tracks']['items'], None)
-#     if track:
-#         print track['name']
-#         print track['uri']
-#     return
+#     track = SpotifyTrack(track_uri)
+#
+#
+#     spotify_plugin.add_track_to_queue(track)
+
+    from soco.plugins.spotify import Spotify
+    from soco.plugins.spotify import SpotifyTrack
+    # create a new plugin, pass the soco instance to it
+    myplugin = Spotify(sonos)
+    print 'index: ' + str(myplugin.add_track_to_queue(SpotifyTrack(track_uri)))
+#     print 'index: ' + str(myplugin.add_album_to_queue(SpotifyAlbum('spotify:album:6a50SaJpvdWDp13t0wUcPU')))
